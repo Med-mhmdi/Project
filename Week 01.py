0420753d343e7302adb4d9b2d63018fb588ad00a
Med-mhmdi <144393444+Med-mhmdi@users.noreply.github.com>
2024-02-19 21:12:40 +0300

    Add files via upload

---
+++ Week 01.py
@@ -0,0 +1,13 @@
+class Solution:
+    def is_prime(self, num):
+        if num < 2:
+            return False
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+    def primesum(self, A):
+        for i in range(2, A):
+            if self.is_prime(i) and self.is_prime(A - i):
+                return [i, A - i]
